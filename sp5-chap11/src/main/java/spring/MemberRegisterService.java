dbb6dab00867007ba83bd8f6bcf05957cd371826
Kang-hg <85867838+Kang-hg@users.noreply.github.com>
2021-08-12 17:40:08 +0900

    Add files via upload

---
+++ sp5-chap11/src/main/java/spring/MemberRegisterService.java
@@ -0,0 +1,36 @@
+package spring;
+
+import java.time.LocalDateTime;
+
+public class MemberRegisterService {
+
+	private MemberDao memberDao;
+	
+	// 생성자 오버로딩
+	public MemberRegisterService()
+	{
+		
+	}
+	
+	public MemberRegisterService(MemberDao memberDao)
+	{
+		this.memberDao = memberDao;
+	}
+	
+	
+	
+	public Long regist(RegisterRequest req)
+	{
+		Member member = memberDao.selectByEmail(req.getEmail());
+		
+		if(member != null)
+			throw new DuplicateMemberException("dup email" + req.getEmail());
+		
+		Member newMember = new Member(req.getEmail(),req.getPassword(),
+				req.getName(),LocalDateTime.now(), req.getBirthdate());
+		memberDao.insert(newMember);
+		return newMember.getId();
+		
+	}
+	
+}
