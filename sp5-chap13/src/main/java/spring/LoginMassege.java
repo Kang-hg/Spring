27b905d5b06d829c68a63b2e14bf3eb9f9d78ef8
Kang-hg <85867838+Kang-hg@users.noreply.github.com>
2021-08-26 17:42:59 +0900

    Add files via upload

---
+++ sp5-chap13/src/main/java/spring/LoginMassege.java
@@ -0,0 +1,15 @@
+package spring;
+
+public class LoginMassege {
+	
+	private String massege;
+
+	public String getMassege() {
+		return massege;
+	}
+
+	public void setMassege(String massege) {
+		this.massege = massege;
+	}
+
+}
---
+++ sp5-chap13/src/main/java/spring/Member.java
@@ -0,0 +1,73 @@
+package spring;
+
+import java.time.LocalDateTime;
+
+public class Member {
+	private Long id;
+	private String email;
+	private String password;
+	private String name;
+	private LocalDateTime registerDateTime;
+	private String birthdate;
+	
+	public Member(String email, String password,
+			String name, LocalDateTime regDateTime, String birthdate)
+	{
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.registerDateTime = regDateTime;
+		this.birthdate = birthdate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public LocalDateTime getRegisterDateTime() {
+		return registerDateTime;
+	}
+	
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public void changePassword(String oldPassword, String newPassword)
+	{
+		if(!password.equals(oldPassword))
+		{
+			throw new WrongIdPasswordException();
+		}
+		this.password = newPassword;
+	}
+	
+	public boolean matchPassword(String password)
+	{
+		return this.password.equals(password);
+	}
+}
+
+
+
+
+
