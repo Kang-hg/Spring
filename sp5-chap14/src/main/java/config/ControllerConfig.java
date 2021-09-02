dffab7e2a4daa01e35341c74a5d49378d9540d01
Kang-hg <85867838+Kang-hg@users.noreply.github.com>
2021-09-02 17:40:20 +0900

    Add files via upload

---
+++ sp5-chap14/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,81 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import controller.ChangePwdController;
+import controller.LoginController;
+import controller.LogoutContoller;
+import controller.MemberListController;
+import controller.RegisterController;
+import spring.AuthService;
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+import survey.SurveyController;
+
+@Configuration
+public class ControllerConfig {
+	
+	@Autowired
+	private MemberRegisterService memberRegSvc;
+	
+	@Autowired
+	private ChangePasswordService changePasswordService;
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	@Autowired
+	private AuthService authService;
+	@Bean
+	public RegisterController registerController()
+	{
+		RegisterController controller = new RegisterController();
+		controller.setMemberRegisterService(memberRegSvc);
+		return controller;
+	}
+	
+	@Bean
+	public SurveyController surveyController()
+	{
+		return new SurveyController();
+	}
+	
+	@Bean
+	public LoginController loginController()
+	{
+		LoginController controller = new LoginController();
+		controller.setAuthService(authService);
+		return controller;
+	}
+	
+	@Bean
+	public LogoutContoller logoutContoller()
+	{
+		return new LogoutContoller();
+	}
+	
+	@Bean
+	public ChangePwdController changePwdController()
+	{
+		ChangePwdController controller = new ChangePwdController();
+		controller.setChangePasswordService(changePasswordService);
+		return controller;
+	}
+	
+	@Bean
+	public MemberListController memberListController()
+	{
+		MemberListController controller = new MemberListController();
+		controller.setMemberDao(memberDao);
+		return controller;
+	}
+}
+
+
+
+
+
+
---
+++ sp5-chap14/src/main/java/spring/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package spring;
+
+public class MemberNotFoundException extends RuntimeException{
+
+	public MemberNotFoundException() {
+	}
+	
+}
